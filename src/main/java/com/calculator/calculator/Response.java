c2ac4e56deda89ef2b897d2b262f633b851baa10
IH-honnette <ihozohonnette12@gmail.com>
2022-12-13 13:19:10 +0200

    Add response handler

---
+++ src/main/java/com/calculator/calculator/Response.java
@@ -0,0 +1,16 @@
+package com.calculator.calculator;
+
+public class Response {
+    double response;
+    public  Response(double response){
+        this.response = response;
+    }
+
+    public Double getResponse() {
+        return response;
+    }
+
+    public void setResponse(Double response) {
+        this.response = response;
+    }
+}
