6abc15373dd6a5a2b201a71b80074502cd19db91
IH-honnette <ihozohonnette12@gmail.com>
2022-12-13 10:26:58 +0200

    Use testng for testing

--- src/test/java/com/calculator/calculator/controller/MathControllerTest.java
+++ src/test/java/com/calculator/calculator/controller/MathControllerTest.java
@@ -1,30 +1,31 @@
 package com.calculator.calculator.controller;
 
-import org.junit.Test;
+import com.calculator.calculator.Response;
+import com.calculator.calculator.dto.DoMathRequestDto;
+import org.testng.annotations.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 public class MathControllerTest {
-   private String url = "/calculate";
 
-   @Autowired
-    private TestRestTemplate restTemplate;
-  
+    @Autowired
+    private  TestRestTemplate restTemplate;
+
+    public MathControllerTest(TestRestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Test
     public void doMath() {
         DoMathRequestDto request = new DoMathRequestDto(1,10, "+");
+        String url = "/calculate";
         ResponseEntity<Response> response = this.restTemplate.postForEntity(url, request, Response.class);
         assertEquals(11,response.getBody().getResponse());
-
     }
 
 }
